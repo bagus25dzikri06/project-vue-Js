63257649cbc897505d45293da80623b184daee18
bagus25dzikri06 <bagusdzikri@gmail.com>
2019-09-11 21:59:37 +0700

    Project situs pertama dengan Bootstrap dan VueJs siap diuji

--- bootstrap-vuejs-1stweb/src/App.vue
+++ bootstrap-vuejs-1stweb/src/App.vue
@@ -1,7 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col>1 of 2</b-col>
+        <b-col>2 of 2</b-col>
+      </b-row>
+
+      <b-row>
+        <b-col>1 of 3</b-col>
+        <b-col>2 of 3</b-col>
+        <b-col>3 of 3</b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
