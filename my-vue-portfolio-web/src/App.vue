1fd0e4c89c3ab73bad68b8a2cc482e666bc3c553
bagus25dzikri06 <bagusdzikri@gmail.com>
2019-09-11 23:42:43 +0700

    Router pada Vue Js sedang diimplementasikan kembali

--- my-vue-portfolio-web/src/App.vue
+++ my-vue-portfolio-web/src/App.vue
@@ -1,5 +1,13 @@
 <template>
-  <div id="app">
-    <router-view/>
+  <div class="abput-card">
+    <div class="card-title">
+      <img src="https://listimg.pinclipart.com/picdir/s/398-3983592_nomnom-sticker-we-bare-bears-png-clipart.png" alt="avatar" class="avatar">
+      <div class ="title-text-content">
+        <p>About Me</p>
+        <h2>John Doe</h2>
+      </div>
+    </div>
+    <div class="card-content"></div>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dolor sed viverra ipsum nunc aliquet bibendum enim. In massa tempor nec feugiat. Nunc aliquet bibendum enim facilisis gravida. Nisl nunc mi ipsum faucibus vitae aliquet nec ullamcorper. Amet luctus venenatis lectus magna fringilla. Volutpat maecenas volutpat blandit aliquam etiam erat velit scelerisque in. Egestas egestas fringilla phasellus faucibus scelerisque eleifend. Sagittis orci a scelerisque purus semper eget duis. Nulla pharetra diam sit amet nisl suscipit. Sed adipiscing diam donec adipiscing tristique risus nec feugiat in. Fusce ut placerat orci nulla. Pharetra vel turpis nunc eget lorem dolor. Tristique senectus et netus et malesuada.</p>
   </div>
 </template>
